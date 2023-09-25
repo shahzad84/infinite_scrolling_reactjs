0d042dc3e36d45c8c1aff7e185e862ab9fe8fd6c
hilarious3 <shahzad20022002@gmail.com>
2023-09-26 01:24:07 +0530

    next commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
+  const [userData, Setdata] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      axios
+        .get(`https://jsonplaceholder.typicode.com/photos?_page=${page + 1}`)
+        .then((response) => {
+          Setdata([...userData, ...response.data]);
+          setPage(page + 1);
+        });
+    }, 500); // Simulating a delay of 500 milliseconds before fetching more data
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => {
+        Setdata(response.data);
+      });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <InfiniteScroll
+      dataLength={userData.length}
+      next={fetchMoreData}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+    >
+      <div className="App">
+        {userData.map((data) => {
+          return (
+            <div className="container my-3">
+              <div className="row">
+                <div className="col-md-4">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img
+                      src={data.thumbnailUrl}
+                      className="card-img-top"
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <p className="card-text">{data.title}</p>
+                    </div>
+                  </div>
+                </div> 
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </InfiniteScroll>
   );
 }
 
